6d64ca500c926b084ee840e65d246e11dc50a23f
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-07 12:55:22 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ j = 0
+ result = []
+
+ while i < collection.length
+    x = yield collection[i]
+   if x == true
+     result[j] = collection[i]
+     j += 1
+   end
+   i += 1
+ end
+  #result.delete(nil)
+  result
 end
